3500ca0c8f0483eabcfc95fb93ed66867fdf078a
Mihail <mik.gryzunov@gmail.com>
2024-04-01 23:56:02 +0300

    Update tmux_bots_stop.sh
    prettify with ShellCheck

--- utils/tmux_bots_stop.sh
+++ utils/tmux_bots_stop.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-# simple stop-script for close multiple running Freq bot's in tmux
+# this is simple stop-script for close multiple running Freq bot's in tmux
+# if you have big db and still see panes, wait - db will close anyway
 
-# time in sec. for save&close database 
-INTERVAL=25
 # set tmux session name
 SESS_NAME='freqtrade_main'
+# time in sec. for save&close database.
+INTERVAL=25
 
 PANES=$(tmux list-panes -t $SESS_NAME 2>/dev/null | wc -l)
 tmux has-session -t $SESS_NAME 2>/dev/null
 SESS_EXIST=$?
 
-if [ $SESS_EXIST -eq 0 ]
-then
-    echo $PANES - Total panes!
-    echo Will be closed at intervals $INTERVAL sec
+if [ $SESS_EXIST -eq 0 ]; then
+    echo "$PANES - Total panes!"
+    echo "Will be closed at intervals $INTERVAL sec"
     pane=0
-    while [ $pane -lt $PANES ]
-    do
-    tmux send-keys -t $SESS_NAME 'C-c' 'C-m'
-    tmux send-keys -t $SESS_NAME 'exit' 'C-m'
-    pane=$(( $pane + 1 ))
-    echo [$pane/$PANES] is closed...
-    sleep $INTERVAL
+    while [ $pane -lt "$PANES" ]; do
+        tmux send-keys -t $SESS_NAME 'C-c' 'C-m'
+        tmux send-keys -t $SESS_NAME 'exit' 'C-m'
+        pane=$(("$pane" + 1))
+        echo [$pane/"$PANES"] is closed...
+        sleep $INTERVAL
     done
-    echo Freqtrade-bots in session $SESS_NAME are closed!
+    echo "Freqtrade-bots in session $SESS_NAME are closed!"
 else
-   echo Look's like you don't have running session with name $SESS_NAME
-   exit 0
+    echo "Look's like you don't have running session with name $SESS_NAME"
+    exit 0
 fi
